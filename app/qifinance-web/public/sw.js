7654bd582efa3924998989970d1e665541a3cca1
qiallyme <crice4485@gmail.com>
2026-07-15 02:05:07 -0500

    push

--- app/qifinance-web/public/sw.js
+++ app/qifinance-web/public/sw.js
@@ -27,7 +27,7 @@ self.addEventListener('fetch', (event) => {
   const request = event.request;
   const url = new URL(request.url);
 
-  if (request.method !== 'GET' || url.pathname.startsWith('/api/') || url.hostname === 'api.fi.qially.com') {
+  if (request.method !== 'GET' || url.pathname.startsWith('/api/') || url.hostname === 'api.fi.qially.com' || url.hostname === 'api.qially.com' || url.hostname.endsWith('workers.dev')) {
     return;
   }
 
